958e0b1047f4d21787494caf342af5871266a8db
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-06-05 17:42:50 -0300

    Dealing with the icon directory change

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,5 +10,6 @@ app.use(express.json())
 app.use(routes)
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+app.use('/public/images', express.static(path.resolve(__dirname, '..', 'public', 'images')))
 
 app.listen(3333)
